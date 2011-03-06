daf511a947e46be71495aab0156f4ec417935d51
Jim <openbip@gmail.com>
2011-03-06 17:23:58 -0500

    list-chat and edit-chat pages.

--- chat/views.py
+++ chat/views.py
@@ -1,1 +1,41 @@
 # Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from chat.models import Chat
+from django import forms
+from django import template
+
+def list_all_chats(request):
+    chats = Chat.objects.all()
+    print "Found %i chats" % chats.count()
+    
+    list_template = template.loader.get_template('chat_list.html')
+    return HttpResponse(list_template.render(template.Context({ 'chats': chats })))
+
+
+class ChatForm(forms.Form):
+    title = forms.CharField(40, 1)
+    description = forms.CharField(300, 1,
+                                  widget=forms.Textarea(attrs = {
+                                                                'rows': '3',
+                                                                'cols': '20'
+                                                                 }))
+
+def edit_chat(request):
+    if request.method == 'POST':
+        chat_form = ChatForm(request.POST)
+        
+        if chat_form.is_valid():
+            data = chat_form.cleaned_data
+            
+            new_chat = Chat()
+            new_chat.title = data.get('title')
+            new_chat.description = data.get('description')
+            
+            new_chat.save()
+            
+            return HttpResponseRedirect('/chats/')
+
+    elif request.method == 'GET':
+        chat_template = template.loader.get_template('chat_edit.html')
+        response_html = chat_template.render(template.RequestContext(request, {'chat_form':ChatForm()}))
+        return HttpResponse(response_html)
--- urls.py
+++ urls.py
@@ -1,15 +1,19 @@
 from django.conf.urls.defaults import patterns
 from django.conf import settings
 from views import welcome
+from chat.views import list_all_chats, edit_chat
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    ('^$', welcome),
     ('^welcome/$', welcome),
     (r'^static-content/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.STATIC_DOC_ROOT })
+        {'document_root': settings.STATIC_DOC_ROOT }),
+    ('^chats/$', list_all_chats),
+    ('^chat/new/$', edit_chat)
 
     # Example:
     # (r'^translette/', include('translette.foo.urls')),
